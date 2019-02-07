8392414d9e6c422cea6e4edc7f5e57dec53fe1d5
Bragi Arnason <25616889+gbragi@users.noreply.github.com>
2019-02-07 23:56:17 +0000

    add windows boxstarter

---
+++ windows/boxstarter.ps1
@@ -0,0 +1,247 @@
+# Get Updates
+Install-WindowsUpdate -acceptEula
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Configure Windows
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+
+Update-ExecutionPolicy Unrestricted
+
+Disable-BingSearch
+Disable-GameBarTips
+
+Set-TaskbarOptions -Size Small -Dock Bottom -Combine Full -Lock
+Set-TaskbarOptions -Size Small -Dock Bottom -Combine Full -AlwaysShowIconsOn
+
+cinst -y Microsoft-Hyper-V-All -source windowsFeatures
+# cinst -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+# Packages
+cinst -y hyper
+
+## Git
+cinst -y git.install
+cinst -y poshgit
+
+# Restart PowerShell / CMDer before moving on - or run
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+cinst Git-Credential-Manager-for-Windows
+cinst github-desktop
+
+## Node, npm
+cinst -y nodejs.install
+npm install -g npm-windows-upgrade
+
+## Editors
+cinst -y vscode
+
+## Visual Studio 2017
+# cinst -y visualstudio2017community
+if (Test-PendingReboot) { Invoke-Reboot }
+
+## Ruby, Go, Python
+cinst dotnetcore-sdk 
+cinst golang
+cinst -y python
+cinst -y pip
+pip install --user pipenv
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+## Devops
+cinst docker-for-windows
+# comes with docker-for-windows
+# cinst kubernetes-cli
+
+cinst azure-cli
+
+cinst awscli
+cinst awstools.powershell
+
+## Basics
+cinst -y firefox
+# cinst -y 7zip.install
+# cinst -y sysinternals
+# cinst -y DotNet3.5
+
+# cinst -y DotNet4.0 -- not needed on windows 8
+# cinst -y DotNet4.5 -- not needed on windows 10
+# cinst -y PowerShell -- not needed on windows 10
+if (Test-PendingReboot) { Invoke-Reboot }
+
+
+$apps = @(
+    # default Windows 10 apps
+    "Microsoft.3DBuilder"
+    "Microsoft.Appconnector"
+    "Microsoft.Advertising.Xaml" 
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingTranslator"
+    "Microsoft.BingWeather"
+    "Microsoft.FreshPaint"
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftPowerBIForWindows"
+    "Microsoft.MinecraftUWP"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.Office.OneNote"
+    "Microsoft.OneConnect"
+    "Microsoft.People"
+    "Microsoft.Print3D"
+    "Microsoft.SkypeApp"
+    "Microsoft.Wallet"
+    #"Microsoft.Windows.Photos"
+    "Microsoft.WindowsAlarms"
+    #"Microsoft.WindowsCalculator"
+    "Microsoft.WindowsCamera"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    "Microsoft.WindowsSoundRecorder"
+    #"Microsoft.WindowsStore"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    
+    
+    # Threshold 2 apps
+    "Microsoft.CommsPhone"
+    "Microsoft.ConnectivityStore"
+    "Microsoft.GetHelp"
+    "Microsoft.Getstarted"
+    "Microsoft.Messaging"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.WindowsFeedbackHub"
+
+    # Creators Update apps
+    "Microsoft.Microsoft3DViewer"
+    #"Microsoft.MSPaint"
+
+    #Redstone apps
+    "Microsoft.BingFoodAndDrink"
+    "Microsoft.BingTravel"
+    "Microsoft.BingHealthAndFitness"
+    "Microsoft.WindowsReadingList"
+
+    # Redstone 5 apps
+    "Microsoft.MixedReality.Portal"
+    "Microsoft.ScreenSketch"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.YourPhone"
+
+    # non-Microsoft
+    "9E2F88E3.Twitter"
+    "PandoraMediaInc.29680B314EFC2"
+    "Flipboard.Flipboard"
+    "ShazamEntertainmentLtd.Shazam"
+    "king.com.CandyCrushSaga"
+    "king.com.CandyCrushSodaSaga"
+    "king.com.BubbleWitch3Saga"
+    "king.com.*"
+    "ClearChannelRadioDigital.iHeartRadio"
+    "4DF9E0F8.Netflix"
+    "6Wunderkinder.Wunderlist"
+    "Drawboard.DrawboardPDF"
+    "2FE3CB00.PicsArt-PhotoStudio"
+    "D52A8D61.FarmVille2CountryEscape"
+    "TuneIn.TuneInRadio"
+    "GAMELOFTSA.Asphalt8Airborne"
+    "TheNewYorkTimes.NYTCrossword"
+    "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+    "Facebook.Facebook"
+    "flaregamesGmbH.RoyalRevolt2"
+    "Playtika.CaesarsSlotsFreeCasino"
+    "A278AB0D.MarchofEmpires"
+    "KeeperSecurityInc.Keeper"
+    "ThumbmunkeysLtd.PhototasticCollage"
+    "XINGAG.XING"
+    "89006A2E.AutodeskSketchBook"
+    "D5EA27B7.Duolingo-LearnLanguagesforFree"
+    "46928bounde.EclipseManager"
+    "ActiproSoftwareLLC.562882FEEB491" # next one is for the Code Writer from Actipro Software LLC
+    "DolbyLaboratories.DolbyAccess"
+    "SpotifyAB.SpotifyMusic"
+    "A278AB0D.DisneyMagicKingdoms"
+    "WinZipComputing.WinZipUniversal"
+    "CAF9E577.Plex"  
+    "7EE7776C.LinkedInforWindows"
+    "613EBCEA.PolarrPhotoEditorAcademicEdition"
+    "Fitbit.FitbitCoach"
+    "DolbyLaboratories.DolbyAccess"
+    "Microsoft.BingNews"
+    "NORDCURRENT.COOKINGFEVER"
+
+    
+    # Additional packages
+    "*Dropbox*"
+    "*Facebook*"
+    "*Dell*"
+    "Microsoft.GetStarted"
+    "*Keeper*"
+
+
+    # apps which cannot be removed using Remove-AppxPackage
+    #"Microsoft.BioEnrollment"
+    #"Microsoft.MicrosoftEdge"
+    #"Microsoft.Windows.Cortana"
+    #"Microsoft.WindowsFeedback"
+    #"Microsoft.XboxGameCallableUI"
+    #"Microsoft.XboxIdentityProvider"
+    #"Windows.ContactSupport"
+)
+
+foreach ($app in $apps) {
+    Write-Output "Trying to remove $app"
+
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
+
+    Get-AppXProvisionedPackage -Online |
+        Where-Object DisplayName -EQ $app |
+        Remove-AppxProvisionedPackage -Online
+}
+
+
+# This script disables unwanted Windows services. If you do not want to disable
+# certain services comment out the corresponding lines below.
+
+$services = @(
+    "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
+    "DiagTrack"                                # Diagnostics Tracking Service
+    "dmwappushservice"                         # WAP Push Message Routing Service (see known issues)
+    "HomeGroupListener"                        # HomeGroup Listener
+    "HomeGroupProvider"                        # HomeGroup Provider
+    "lfsvc"                                    # Geolocation Service
+    "MapsBroker"                               # Downloaded Maps Manager
+    "NetTcpPortSharing"                        # Net.Tcp Port Sharing Service
+    "RemoteAccess"                             # Routing and Remote Access
+    "RemoteRegistry"                           # Remote Registry
+    "SharedAccess"                             # Internet Connection Sharing (ICS)
+    "TrkWks"                                   # Distributed Link Tracking Client
+    "WbioSrvc"                                 # Windows Biometric Service
+    #"WlanSvc"                                 # WLAN AutoConfig
+    "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
+    "wscsvc"                                   # Windows Security Center Service
+    #"WSearch"                                 # Windows Search
+    "XblAuthManager"                           # Xbox Live Auth Manager
+    "XblGameSave"                              # Xbox Live Game Save Service
+    "XboxNetApiSvc"                            # Xbox Live Networking Service
+
+    # Services which cannot be disabled
+    #"WdNisSvc"
+)
+
+foreach ($service in $services) {
+    Write-Output "Trying to disable $service"
+    Get-Service -Name $service | Set-Service -StartupType Disabled
+}
