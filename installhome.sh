0a74b65429f16fdebe6f18003a4ec788d9f09939
Bragi Arnason <25616889+gbragi@users.noreply.github.com>
2019-07-15 00:40:06 +0000

    add nixos support

---
+++ installhome.sh
@@ -0,0 +1,128 @@
+#!/bin/sh
+
+# saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail -o noclobber -o nounset
+
+if [[ $EUID = 0 ]]; then
+    echo "This script must be run as normal user"
+    exit 1
+fi
+
+usage () {
+	echo "Usage: --help, --default, --dotfiles, --code, --dir"
+    exit 1
+}
+
+TEMP=`getopt -o '' --long help,default,dotfiles,code,dir -n 'test.sh' -- "$@"`
+eval set -- "$TEMP"
+
+TOOLS_DIR="$HOME/tools"
+CODE_DIR="$HOME/code"
+
+initdefault() {
+    RUNDOTFILES=$1
+    RUNCODE=$1
+    RUNDIR=$1
+}
+
+HELP=false
+
+initdefault false
+
+while true; do
+    case "$1" in
+        --help) usage; shift ;;
+        --default) initdefault true; shift ;;
+        --dotfiles) RUNDOTFILES=true; shift ;;
+        --code) RUNCODE=true; shift ;;
+        --dir) RUNDIR=true; shift ;;
+        --) shift ; break ;;
+        *) usage ;;
+    esac
+done
+
+# Directory setup
+if [ "$RUNDIR" = true ]; then
+    echo "Setting up directories..."
+    mkdir -p $CODE_DIR/tmp
+    mkdir -p $TOOLS_DIR
+fi
+
+# Install dotfiles
+# make symlinks from the home directory to files in ~/dotfiles
+if [ "$RUNDOTFILES" = true ]; then
+    echo "Setting up dotfiles..."
+    CONFIG_DIR="$HOME/.config"
+    SSH_CONFIG_DIR="$HOME/.ssh"
+    mkdir -p "$SSH_CONFIG_DIR"
+    VSCODE_DIR="$CONFIG_DIR/Code - OSS/User"
+    BACKUP_DIR="./backup"
+    I3_DIR="$CONFIG_DIR/i3"
+    I3_BACKUP="$BACKUP_DIR/i3_backup_config"
+    I3_CONFIG="$I3_DIR/config"
+    PROFILE_CONFIG="$HOME/.profile"
+    PROFILE_BACKUP="$BACKUP_DIR/profile_backup"
+    mkdir -p "$BACKUP_DIR"
+    
+    echo stow vscode
+    mkdir -p "$VSCODE_DIR"
+    stow vscode -t "$VSCODE_DIR"
+    
+    echo stow vim
+    stow vim -t "$HOME"
+
+    echo stow git
+    stow git -t "$HOME"
+    
+    echo stow redshift
+    stow redshift -t "$CONFIG_DIR"
+    
+    if [ -f "$PROFILE_CONFIG" ] && [ ! -L "$PROFILE_CONFIG" ]; then
+        cp "$PROFILE_CONFIG" "$PROFILE_BACKUP"
+    fi
+    
+    echo stow profile
+    if ! grep -q '.profile.customize' ~/.profile; then
+        echo "# Customize profile" >> ~/.profile
+        echo ". ~/.profile.customize" >> ~/.profile
+    fi
+    stow profile -t "$HOME"
+    
+    echo "################### Profile config diff #######################"
+    diff "$PROFILE_BACKUP" "$PROFILE_CONFIG" --color=always || true
+    echo "##########################################################"
+    
+    echo stow i3
+    
+    if [ -f "$I3_CONFIG" ] && [ ! -L "$I3_CONFIG" ]; then
+        mv "$I3_CONFIG" "$I3_BACKUP"
+    fi
+    
+    stow i3 -t "$I3_DIR"
+    
+    echo "################### i3 config diff #######################"
+    diff "$I3_BACKUP" "$I3_CONFIG" --color=always || true
+    echo "##########################################################"
+
+    echo stow ssh config
+    stow ssh -t "$SSH_CONFIG_DIR"
+    
+    # Do a check for all broken symlinks in home directory and print to stdout
+    echo "Listing all broken symlinks"
+    echo $(find $HOME -path ~/go -prune -o -xtype l)
+    
+    echo "All clear...good to go"
+fi
+
+
+# Install vs code extensions
+if [ "$RUNCODE" = true ]; then
+    echo "Installing vs code extensions"
+    readarray -t extensions < ./vscode-extensions.txt
+
+    for i in "${extensions[@]}"
+    do
+        echo "$i"
+        echo "yes" | code --install-extension "$i" || true
+    done
+fi
---
+++ nix/configuration.nix
@@ -0,0 +1,158 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Atlantic/Reykjavik";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs;
+  [
+    zsh
+    oh-my-zsh
+    wget 
+    vim
+    termite
+    firefox-bin
+    pavucontrol
+    blueman
+    vscode
+    git
+    stow
+  ];
+
+  environment.variables.EDITOR = "vim";
+  environment.variables.TERMINAL = "termite";
+
+  fonts.fonts = with pkgs; [
+    hermit
+    source-code-pro
+    terminus_font
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
+  hardware.bluetooth.enable = true;
+
+  # Enable X11 and Desktop management
+  services.xserver = {
+    enable = true;
+    autorun = true;
+    layout = "us,is";
+    xkbOptions = "grp:win_space_toggle";
+    desktopManager = {
+      default = "xfce";
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+    windowManager.i3.enable = true;
+    windowManager.i3.package = pkgs.i3-gaps;
+    displayManager.lightdm.enable = true;
+  };
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable Keyring
+  #services.gnome3.gnome-keyring.enable = true;
+  #services.gnome3.seahorse.enable = true;
+
+  # Enable zsh
+  programs.zsh.enable = true;
+  programs.zsh.interactiveShellInit = ''
+    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+
+    # Customize your oh-my-zsh options here
+    ZSH_THEME="terminalparty"
+    plugins=(git docker)
+
+    HISTFILESIZE=500000
+    HISTSIZE=500000
+    setopt SHARE_HISTORY
+    setopt HIST_IGNORE_ALL_DUPS
+    setopt HIST_IGNORE_DUPS
+    setopt INC_APPEND_HISTORY
+    autoload -U compinit && compinit
+    unsetopt menu_complete
+    setopt completealiases
+
+    if [ -f ~/.aliases ]; then
+      source ~/.aliases
+    fi
+
+    source $ZSH/oh-my-zsh.sh
+  '';
+
+  programs.zsh.promptInit = ""; 
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bragi = {
+    isNormalUser = true;
+    description = "Bragi Arnason";
+    home = "/home/bragi";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "audio" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+
+}
